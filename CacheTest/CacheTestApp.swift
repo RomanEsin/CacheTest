886acb2b881c4169190a70c0b2142700c618e3d3
Roman Esin <esinromanswift@gmail.com>
2022-05-27 00:33:41 +1000

    Initial commit

---
+++ CacheTest/CacheTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheTestApp.swift
+//  CacheTest
+//
+//  Created by Роман Есин on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CacheTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
